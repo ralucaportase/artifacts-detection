c636eeed31f1118e1146807c0ec3dc6101c472a9
Alexandru Silvesan <alexandru.silvesan@ullink.com>
2017-06-19 19:51:53 +0300

    fix multichannel concurrency
    Change-Id: If9f6971ddbdb8a060cb74774fea101a52f4aa744

--- business/src/main/java/edu/utcn/eeg/artifactdetection/input/segmentation/MultiChannelFileProcessorThread.java
+++ business/src/main/java/edu/utcn/eeg/artifactdetection/input/segmentation/MultiChannelFileProcessorThread.java
@@ -7,48 +7,81 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
-
 import org.apache.log4j.Logger;
-
+import edu.utcn.eeg.artifactdetection.builders.StructureBuilder;
 import edu.utcn.eeg.artifactdetection.model.Configuration;
+import edu.utcn.eeg.artifactdetection.model.Feature;
 import edu.utcn.eeg.artifactdetection.model.Segment;
 
 public class MultiChannelFileProcessorThread implements Callable<List<Segment>>{
 
-	private Logger logger = LoggerUtil.logger(MultiChannelFileProcessorThread.class);
-	private File file;
-	private int channelNr;
-	
-	public MultiChannelFileProcessorThread(File file, int channelNr){
-		this.file = file;
-		this.channelNr = channelNr;
-	}
-	
-	private List<Segment> createSegments(double[] data){
-		List<Segment> segments = new ArrayList<>(); 
-		for(int i =0; i<Configuration.WINDOW_SIZE; i+=Configuration.STEP){
-			for(int j=i; j<data.length; j+=Configuration.WINDOW_SIZE){
-				double[] values = Arrays.copyOfRange(data, j, j+Configuration.WINDOW_SIZE);
-				Segment segment = new Segment(values);
-				segment.setInitIdx(j);
-				segment.setIterIdx(i);
-				segment.setChannelNr(channelNr);
-				segments.add(segment);
-			}
-		}
-		return segments;
-	}
+    private final Logger logger = LoggerUtil.logger(MultiChannelFileProcessorThread.class);
+    private final File             file;
+    private final int              channelNr;
+    private final StructureBuilder structureBuilder;
+
+    public MultiChannelFileProcessorThread(File file, int channelNr, StructureBuilder structureBuilder)
+    {
+        this.file = file;
+        this.channelNr = channelNr;
+        this.structureBuilder = structureBuilder;
+    }
+
+    private List<Segment> createSegments(double[] data)
+    {
+        this.logger.warn("Started creating segments for file  " + this.file);
+        List<Segment> segments = new ArrayList<>();
+        for (int i = 0; i < Configuration.WINDOW_SIZE; i += Configuration.STEP)
+        {
+            for (int j = i; j < data.length; j += Configuration.WINDOW_SIZE)
+            {
+                this.logger.warn("File " + this.channelNr + " Iteratie " + i + " Init " + j);
+                double[] values = Arrays.copyOfRange(data, j, j + Configuration.WINDOW_SIZE);
+                Segment segment = new Segment(values);
+                segment.setInitIdx(j);
+                segment.setIterIdx(i);
+                segment.setChannelNr(this.channelNr);
+
+                Feature[] features = this.structureBuilder.computeMultiRegionFeaturesForSegment(segment.getValues());
+                segment.setFeatures(features);
+                segment.setCorrectType(this.structureBuilder.computeCorrectType(segment.getInitIdx(), segment.getChannelNr()));
+                this.logger.warn("Processing segment: " + segment + " Nr elemente " + segment.getValues().length + " Features " + Arrays.toString(segment.getFeatures()));
+                segment.setValues(null);
+                segments.add(segment);
+            }
+        }
+        this.logger.warn("Finished creating segments for file  " + this.file);
+        return segments;
+    }
 
 	@Override
 	public List<Segment> call() throws Exception {
 		List<Double> data = new ArrayList<>();
-		try(Scanner scan = new Scanner(file)){
-			while (scan.hasNextDouble()) {
-				data.add(scan.nextDouble());
-			}
-		} catch (FileNotFoundException e) {
-			logger.error("A problem occured while reading file "+file.getName(),e);
-		}
-		return createSegments(data.stream().mapToDouble(i -> i).toArray());
-	}
+        this.logger.warn("Started scan for file  " + this.file + " thread is " + Thread.currentThread()
+                                                                                       .getId());
+        int count = 0;
+        try (Scanner scan = new Scanner(this.file))
+        {
+            while (scan.hasNextDouble())
+            {
+                count++;
+                if (count <= Configuration.TRAIN_MAX_INDEX)
+                {
+                    data.add(scan.nextDouble());
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            this.logger.error("A problem occured while reading file " + this.file.getName(), e);
+        }
+        this.logger.warn("Ended scan for file  " + this.file);
+        return createSegments(data.stream()
+                                  .mapToDouble(i -> i)
+                                  .toArray());
+    }
 }
--- business/src/main/java/edu/utcn/eeg/artifactdetection/input/segmentation/MultiChannelMain.java
+++ business/src/main/java/edu/utcn/eeg/artifactdetection/input/segmentation/MultiChannelMain.java
@@ -7,72 +7,85 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-
+import org.apache.log4j.Logger;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
-
 import edu.utcn.eeg.artifactdetection.builders.StructureBuilder;
 import edu.utcn.eeg.artifactdetection.model.Configuration;
-import edu.utcn.eeg.artifactdetection.model.Feature;
 import edu.utcn.eeg.artifactdetection.model.MultiChannelSegment;
 import edu.utcn.eeg.artifactdetection.model.MultiRegionSegmentKey;
 import edu.utcn.eeg.artifactdetection.model.Segment;
 
 public class MultiChannelMain {
 
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService executor = Executors.newFixedThreadPool(128);
+    public static void main(String[] args) throws Exception
+    {
+        MultiChannelFeaturesExtractor multiChannelFeaturesExtractor = new MultiChannelFeaturesExtractor();
+        LoggerUtil.configure();
+        Logger logger = LoggerUtil.logger(MultiChannelMain.class);
+
+        ExecutorService executor = Executors.newFixedThreadPool(8);
         Map<Integer,Future<List<Segment>>> results = new HashMap<>();
 		File folder = new File(Configuration.INPUT_FILES);
-		for (final File fileEntry : folder.listFiles()) {
-	        if (!fileEntry.isDirectory()) {
-	        	int channelNr = getChannelFromFile(fileEntry.getName());
-	            results.put(new Integer(channelNr), executor.submit(new MultiChannelFileProcessorThread(fileEntry,channelNr)));
-	        }
-	    }
+        StructureBuilder structureBuilder = new StructureBuilder();
+        Arrays.stream(folder.listFiles())
+              .filter(fileEntry -> !fileEntry.isDirectory())
+              .limit(2)
+              .forEach(fileEntry ->
+              {
+                  int channelNr = getChannelFromFile(fileEntry.getName());
+                  results.put(new Integer(channelNr), executor.submit(new MultiChannelFileProcessorThread(fileEntry, channelNr, structureBuilder)));
+              });
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+
 		Multimap<MultiRegionSegmentKey, Segment> multimap = ArrayListMultimap.create();
-		StructureBuilder structureBuilder = new StructureBuilder();
+
 		for (Entry<Integer, Future<List<Segment>>> result: results.entrySet()) {
 			for (Segment segment : result.getValue().get()) {
-				Feature[] features = structureBuilder.computeMultiRegionFeaturesForSegment(segment.getValues());
-				segment.setFeatures(features);
-				segment.setCorrectType(structureBuilder.computeCorrectType(segment.getInitIdx(), segment.getChannelNr()));
-				MultiRegionSegmentKey key = segment.getMultiRegionSegmentKey();
-				multimap.put(key, segment);
-			}
-		}
-		List<MultiChannelSegment> multiChannelSegments = new ArrayList<>();
-		for (MultiRegionSegmentKey key: multimap.keySet()) {
-			Collection<Segment> segments = multimap.get(key);
-			if(segments.size()>=120){
-				ArrayList<Segment> segmentsArray = new ArrayList<>(segments);
-				MultiChannelSegment multiChannelSegment = new MultiChannelSegment(segmentsArray);
-				multiChannelSegment.setFeatures(structureBuilder.computeMultiRegionFeaturesForMultiChannelSegment(segmentsArray));
-				multiChannelSegments.add(multiChannelSegment);
-			}
-		}
-		
-		executor.awaitTermination(1, TimeUnit.DAYS);
-	}
-	
+                logger.warn("Processing segment: " + segment + " Features " + segment.getFeatures());
+                MultiRegionSegmentKey key = segment.getMultiRegionSegmentKey();
+                multimap.put(key, segment);
+            }
+        }
 
-	
-	private static int getChannelFromFile(String file){
-		String delims = "[._]+";
-		String[] tokens = file.split(delims);
-		for(String token:tokens){
-			try{
-				return Integer.parseInt(token);
-			}catch(NumberFormatException e){
-				continue;
-			}
-		}
-		return -1;
-	}
+        List<MultiChannelSegment> multiChannelSegments = new ArrayList<>();
+        for (MultiRegionSegmentKey key : multimap.keySet())
+        {
+            Collection<Segment> segments = multimap.get(key);
+            if (segments.size() >= 1)
+            {
+                ArrayList<Segment> segmentsArray = new ArrayList<>(segments);
+                MultiChannelSegment multiChannelSegment = new MultiChannelSegment(segmentsArray);
+                multiChannelFeaturesExtractor.setMultiChannelFeatureForAllSegments(multiChannelSegment);
+                multiChannelSegment.setFeatures(structureBuilder.computeMultiRegionFeaturesForMultiChannelSegment(segmentsArray));
+                multiChannelSegments.add(multiChannelSegment);
+                logger.warn(multiChannelSegment);
+            }
+        }
+    }
+
+    private static int getChannelFromFile(String file)
+    {
+        String delims = "[._]+";
+        String[] tokens = file.split(delims);
+        for (String token : tokens)
+        {
+            try
+            {
+                return Integer.parseInt(token);
+            }
+            catch (NumberFormatException e)
+            {
+                continue;
+            }
+        }
+        return -1;
+    }
 
 }
